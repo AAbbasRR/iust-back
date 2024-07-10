dadf7de54ad7f7e8dc7a6a491d85701cd44bfcd3
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2024-07-10 12:15:11 +0330

    reafactor: change key of urlhanddler errors

--- src/utils/url_handlers.py
+++ src/utils/url_handlers.py
@@ -5,11 +5,11 @@ from .base_errors import BaseErrors
 
 def custom_404_response(request, exception):
     return HttpResponseNotFound(
-        JsonResponse({"status": False, "message": BaseErrors.url_not_found})
+        JsonResponse({"status": False, "detail": BaseErrors.url_not_found})
     )
 
 
 def custom_500_response(request):
     return HttpResponseServerError(
-        JsonResponse({"status": False, "message": BaseErrors.server_error})
+        JsonResponse({"status": False, "detail": BaseErrors.server_error})
     )
