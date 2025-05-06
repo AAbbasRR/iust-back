729775794eee72406c226ab3648f20d8189f05b4
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2025-05-06 23:49:56 +0330

    change max length

--- src/utils/general_models.py
+++ src/utils/general_models.py
@@ -82,9 +82,9 @@ class GeneralEducationModel(GeneralAddressModel):
 
 
 class GeneralMultiLanguageModel(GeneralAddressModel):
-    fa_name = models.CharField(max_length=64, verbose_name=_("Faculty Name"))
-    en_name = models.CharField(max_length=64, verbose_name=_("English Name"))
-    ar_name = models.CharField(max_length=64, verbose_name=_("Arabic Name"))
+    fa_name = models.CharField(max_length=128, verbose_name=_("Faculty Name"))
+    en_name = models.CharField(max_length=128, verbose_name=_("English Name"))
+    ar_name = models.CharField(max_length=128, verbose_name=_("Arabic Name"))
 
     class Meta:
         abstract = True
