5d35768500da1827b276b61864a38d93f0f706c4
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2025-05-06 23:22:12 +0330

    fix filter faaculty

--- src/app_education/api/views/field_of_study_admin.py
+++ src/app_education/api/views/field_of_study_admin.py
@@ -19,7 +19,7 @@ class AdminFieldOfStudyListCreateView(generics.ListCreateAPIView):
     pagination_class = BasePagination
     serializer_class = AdminFieldOfStudySerializer
     queryset = FieldOfStudyModel.objects.all()
-    filter_fields = "faculty"
+    filterset_fields = ["faculty"]
 
 
 class AdminFieldOfStudyUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
